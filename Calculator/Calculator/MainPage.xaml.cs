fbad9eba32556b2a420059b5577c970690bfeec6
Jaden Binette <binettejaden@gmail.com>
2023-12-12 19:33:01 -0500

    Add project files.

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public string answerString = "Calc Demo";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Add_Nums(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(TopEnt.Text);
+            double secondNum = double.Parse(BotEnt.Text);
+
+            topAns.Text = (firstNum + secondNum).ToString();
+            
+        }
+        void Sub_Nums(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(TopEnt.Text);
+            double secondNum = double.Parse(BotEnt.Text);
+
+            topAns.Text = (firstNum - secondNum).ToString();
+        }
+        void Div_Nums(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(TopEnt.Text);
+            double secondNum = double.Parse(BotEnt.Text);
+
+            topAns.Text = (firstNum / secondNum).ToString();
+        }
+        void Mul_Nums(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(TopEnt.Text);
+            double secondNum = double.Parse(BotEnt.Text);
+
+            topAns.Text = (firstNum * secondNum).ToString();
+        }
+    }
+}
